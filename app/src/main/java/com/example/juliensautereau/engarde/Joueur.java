02d922de8276ea50eca2fc084d5f000360a80928
Anja <anja.tabard@gmail.com>
2018-02-05 10:09:26 +0100

    ajout classe jeu

---
+++ app/src/main/java/com/example/juliensautereau/engarde/Joueur.java
@@ -0,0 +1,11 @@
+package com.example.juliensautereau.engarde;
+
+import java.util.ArrayList;
+
+
+public class Joueur {
+    Boolean statut;
+    int score;
+    int position;
+    ArrayList<Integer> cartes;
+}
