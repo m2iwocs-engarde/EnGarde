7ae79882b3de43123a0223b09d56bff8e28e1a3d
Anja <anja.tabard@gmail.com>
2018-02-05 10:40:10 +0100

    ajout fonctions jeu

--- app/src/main/java/com/example/juliensautereau/engarde/Jeu.java
+++ app/src/main/java/com/example/juliensautereau/engarde/Jeu.java
@@ -6,7 +6,12 @@ import java.util.ArrayList;
 
 
 public class Jeu extends AppCompatActivity{
-    ArrayList<Integer> cartes = new ArrayList<Integer>();
+    ArrayList<Integer> pioche = new ArrayList<Integer>();
+    Integer defausseLast = 0;
+    Joueur j1;
+    Joueur j2;
+    Joueur attaquant;
+    Joueur defenseur;
 
     public void initialisation(){
         initPioche();
@@ -23,24 +28,54 @@ public class Jeu extends AppCompatActivity{
 
         while(!tmp.isEmpty()){
             rnd = (int) (Math.random()*tmp.size());
-            cartes.add(tmp.get(rnd));
+            pioche.add(tmp.get(rnd));
             tmp.remove(rnd);
         }
     }
 
     public void creerJoueur(){
         ArrayList<Integer> tmp = new ArrayList<Integer>();
-        for(int i = 1 ; i <= 5 ; i++){
-            tmp.add(i);tmp.add(i);tmp.add(i);tmp.add(i);tmp.add(i);
+        for(int i = 0 ; i < 5 ; i++){
+            tmp.add(pioche.get(0));
+            pioche.remove(0);
         }
+        j1 = new Joueur(true,true,0,1,tmp);
+        tmp.clear();
 
-        int rnd;
-
-        while(!tmp.isEmpty()){
-            rnd = (int) (Math.random()*tmp.size());
-            cartes.add(tmp.get(rnd));
-            tmp.remove(rnd);
+        for(int i = 0 ; i < 5 ; i++){
+            tmp.add(pioche.get(0));
+            pioche.remove(0);
         }
+        j2 = new Joueur(false,false, 0,23,tmp);
     }
+
+    public Boolean avancer(Integer carte){
+        if(attaquant.getSens()){
+            if(attaquant.getPosition() + carte >= defenseur.getPosition()){
+                //on depasse l'adversaire ou meme case
+                return false; //interdiction d'avancer et de jouer la carte, il faut en choisir une autre
+            }
+            attaquant.setPosition(attaquant.getPosition() + carte);//coup valide
+
+        }else{
+            if(attaquant.getPosition() - carte >= defenseur.getPosition()){
+                //on depasse l'adversaire ou meme case
+                return false; //interdiction d'avancer et de jouer la carte, il faut en choisir une autre
+            }
+            attaquant.setPosition(attaquant.getPosition() - carte);//coup valide
+        }
+
+        attaquant.removeCartes(carte);
+        defausseLast = carte;
+
+        //fin du tour + envoi position
+
+        return true;
+
+    }
+
+
+
+
 }
 
--- app/src/main/java/com/example/juliensautereau/engarde/Joueur.java
+++ app/src/main/java/com/example/juliensautereau/engarde/Joueur.java
@@ -4,8 +4,63 @@ import java.util.ArrayList;
 
 
 public class Joueur {
-    Boolean statut;
-    int score;
-    int position;
-    ArrayList<Integer> cartes;
+    private Boolean statut;
+    private Boolean sens;
+    private int score;
+    private int position;
+    private ArrayList<Integer> cartes;
+
+    public Joueur(Boolean statut, Boolean sens, int score, int position, ArrayList<Integer> cartes) {
+        this.statut = statut;
+        this.sens = sens;
+        this.score = score;
+        this.position = position;
+        this.cartes = cartes;
+    }
+
+    public Boolean getSens() {
+        return sens;
+    }
+
+    public void setSens(Boolean sens) {
+        this.sens = sens;
+    }
+
+    public Boolean getStatut() {
+        return statut;
+    }
+
+    public void setStatut(Boolean statut) {
+        this.statut = statut;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public ArrayList<Integer> getCartes() {
+        return cartes;
+    }
+
+    public void setCartes(ArrayList<Integer> cartes) {
+        this.cartes = cartes;
+    }
+
+    public void removeCartes(Integer carte) {
+        this.cartes.remove(this.cartes.indexOf(carte));
+    }
 }
+
+
