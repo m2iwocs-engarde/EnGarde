cfa8f110fc1541663bd0cc79defe7fd4d5a8a079
Anja <anja.tabard@gmail.com>
2018-02-05 10:17:31 +0100

    ajout classe jeu

--- app/src/main/java/com/example/juliensautereau/engarde/Jeu.java
+++ app/src/main/java/com/example/juliensautereau/engarde/Jeu.java
@@ -10,9 +10,25 @@ public class Jeu extends AppCompatActivity{
 
     public void initialisation(){
         initPioche();
+        creerJoueur();
     }
 
     public void initPioche(){
+        ArrayList<Integer> tmp = new ArrayList<Integer>();
+        for(int i = 1 ; i <= 5 ; i++){
+            tmp.add(i);tmp.add(i);tmp.add(i);tmp.add(i);tmp.add(i);
+        }
+
+        int rnd;
+
+        while(!tmp.isEmpty()){
+            rnd = (int) (Math.random()*tmp.size());
+            cartes.add(tmp.get(rnd));
+            tmp.remove(rnd);
+        }
+    }
+
+    public void creerJoueur(){
         ArrayList<Integer> tmp = new ArrayList<Integer>();
         for(int i = 1 ; i <= 5 ; i++){
             tmp.add(i);tmp.add(i);tmp.add(i);tmp.add(i);tmp.add(i);
