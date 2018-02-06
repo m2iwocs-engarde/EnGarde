158fa8563db6d7b09c549117496e478a9ce0c333
Anja <anja.tabard@gmail.com>
2018-02-06 13:29:58 +0100

    ajout de commentaires, ajout des erreurs

--- app/src/main/java/com/example/juliensautereau/engarde/Jeu.java
+++ app/src/main/java/com/example/juliensautereau/engarde/Jeu.java
@@ -1,9 +1,11 @@
 package com.example.juliensautereau.engarde;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
+//TODO : bouton fin du tour
 
 public class Jeu extends AppCompatActivity{
     ArrayList<Integer> pioche = new ArrayList<Integer>();
@@ -31,8 +33,6 @@ public class Jeu extends AppCompatActivity{
 
         }else{
             //j2
-
-            //j2
             String brut = receiveData();
             ArrayList<String> msg = parse(brut);
             if(msg.get(0).equals("J")){
@@ -42,6 +42,8 @@ public class Jeu extends AppCompatActivity{
                 }
             }else{
                 //TODO ERROR
+                Toast.makeText(this,"Echec de l'initialisation J2",Toast.LENGTH_SHORT).show();
+
             }
 
             sendData("O:1");
@@ -53,6 +55,7 @@ public class Jeu extends AppCompatActivity{
                 j1 = new Joueur(Boolean.parseBoolean(msg2.get(1)),Boolean.parseBoolean(msg2.get(2)));
             }else{
                 //TODO ERROR
+                Toast.makeText(this,"Echec de l'initialisation J1",Toast.LENGTH_SHORT).show();
             }
 
             sendData("O:2");
@@ -77,6 +80,56 @@ public class Jeu extends AppCompatActivity{
         }
     }
 
+    public ArrayList<String> parse(String message){
+        String[] original = message.split(":");
+        String[] contenu = original[1].split(";");
+
+        ArrayList<String> messageArray = new ArrayList<String>();
+
+        messageArray.add(original[0]);
+        for(int i=0; i <contenu.length;i++){
+            messageArray.add(contenu[i]);
+        }
+
+        return messageArray;
+    }
+
+    public void test(ArrayList<String> msg){
+
+        switch(msg.get(0)){
+            case "D" : // Mise a jour de la pile de défausse
+                defausseLast = Integer.parseInt(msg.get(1));
+                break;
+
+            case "P" : // Joueur 2 demmande a piocher
+                String ret = "PR:";
+                for(Integer i : pioche(j2)){
+                    ret += i + ";";
+                }
+
+                ret = ret.substring(0,ret.length()-1);
+                sendData(ret);
+
+                String brut = receiveData(); //TODO Verif Syncro 01
+                sendData("PN:" + pioche.size());
+                break;
+            case "PN" : // Mise a jour de la taille de la pioche
+                this.taillePioche = Integer.parseInt(msg.get(1));
+                break;
+            case "PR" : // J2 recoit ses cartes
+                for(int i=1; i<msg.size();i++) {
+                    j2.addCartes(Integer.parseInt(msg.get(i)));
+                }
+                sendData("OK"); //TODO Verif Syncro 01
+                break;
+
+            case "PA" : //Affiche le message :"Le coup a été parrer"
+                //TODO
+                break;
+
+        }
+    }
+
     /*-----------------------------------------------------------------------------------------------*/
 
     /*-----------------------Début du tour--------------------------------------------------------------*/
@@ -88,11 +141,11 @@ public class Jeu extends AppCompatActivity{
     public void tourActif(){
         boolean parrade,reculer;
 
-        parrade = peutParrer; //verification que le joueur peut parrer
+        parrade = peutParrer(cartes); //verification que le joueur peut parrer
 
         if(attaquerIndirect){ //si le joueur vient d'etre attaque indirectement
             //TODO Afficher bouton griser Reculer et Parrer
-            reculer = peutReculer;//vérification que le joueur peut reculer
+            reculer = peutReculer();//vérification que le joueur peut reculer
 
 
             //on ne degrise que les actions possibles
@@ -124,11 +177,11 @@ public class Jeu extends AppCompatActivity{
     public void tourAttaque(){
         boolean attaquer,avancer,reculer;
 
-        attaquer = peutAttaquer();
-        avancer = peutAvancer();
-        reculer = peutReculer();
+        attaquer = peutAttaquer();//vérification que le joueur peut attaquer
+        avancer = peutAvancer();//vérification que le joueur peut avancer
+        reculer = peutReculer();//vérification que le joueur peut reculer
 
-        //TODO affichage bt
+        //TODO affichage bts
 
         if(attaquer){
             //Dégriser bouton attaquer
@@ -141,7 +194,7 @@ public class Jeu extends AppCompatActivity{
         }
 
 
-        if(!attaquer && !avancer && !reculer){
+        if(!attaquer && !avancer && !reculer){//si aucune action possible, le joeuur a perdu
             //Perdu
         }
 
@@ -153,112 +206,163 @@ public class Jeu extends AppCompatActivity{
 
         }
     }
+    /*------------------------------------------------------------------------------------------*/
 
-    /* --- Deb actionButton --- */
+    /*---------------------Verification des actions --------------------------------------------*/
 
-    public void actionButtonParrer(){
-        boolean fin = false;
-
-
-        fin = parade(getCarteSelectioner());
-        if(!fin){
-            //Erreur msg: parrade doit étre = a attaque
+    public boolean peutParrer(ArrayList<Integer> cartes){
+        int nb = 0;
+        for(int c : actif.getCartes()){
+            if(c == cartes.get(0)){
+                nb++;
+            }
         }
 
-        if(fin){
+        //la parade est possible
+        if (nb >= cartes.size()) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean peutReculer(){
+        if(actif.getSens()){
+            for(Integer c : actif.getCartes()){
+                if(actif.getPosition() - c >= 1){
+
+                    return true; //une carte est possible
+                }
+            }
+
+        }else{
+            for(Integer c : actif.getCartes()) {
+                if (actif.getPosition() + c <= 23) {
+                    return true;
+                }
+            }
+        }
+        return false; //aucune carte ne peut faire reculer le joueur
+    }
+
+    public Boolean peutAvancer(){
+        if(actif.getSens()){
+            for(Integer c : actif.getCartes()){
+                if(actif.getPosition() + c < inactif.getPosition()){
+                    return true; //une cart est possible
+                }
+            }
+
+        }else{
+            for(Integer c : actif.getCartes()) {
+                if (actif.getPosition() - c > inactif.getPosition()) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+    }
+
+    public Boolean peutAttaquer(){
+        for(Integer c : actif.getCartes()) {
+            if (Math.abs(actif.getPosition() - inactif.getPosition()) == c) {
+                return true; //une carte est a la bonne distance
+            }
+        }
+        return false; //aucune carte ne permet d'attaquer
+    }
+
+    /*------------------------------------------------------------------------------------------*/
+
+    /* --------------------------------- Deb actionButton ------------------------------------- */
+
+    public void actionButtonParrer(){
+        if(parade(getCarteSelectioner())){
             String msg = "PA:";
             messages.add(msg);
-
             tourAttaque();
+        }else {
+            //Erreur msg: parrade doit être = a attaque
+            Toast.makeText(this,"La parade a échoué",Toast.LENGTH_SHORT).show();
         }
 
     }
 
     public void actionButtonAttaquer(){
-        boolean fin = false;
+        ArrayList<Integer> carteSelectionée = getCarteSelectioner();
 
-        ArrayList<Integer> tmp = getCarteSelectioner();
-        fin = attaquer(tmp);
-
-        if(fin){
-
+        if(attaquer(carteSelectionée)){
             String msg = "AD:";
-
-            for(Integer i : tmp){
-                msg += tmp + ";";
+            for(Integer i : carteSelectionée){
+                msg += carteSelectionée + ";";
             }
-
             msg = msg.substring(0,msg.length()-1);
-
             messages.add(msg);
-
             finTour();
         }else{
             //Erreur msg: attaque imposible
+            Toast.makeText(this,"L'attaque a échoué",Toast.LENGTH_SHORT).show();
+            return;
         }
     }
 
     public void actionButtonAttaquerIndirectement(){
-        boolean fin = false;
+        ArrayList<Integer> carteSelectionée = getCarteSelectioner();
 
-        ArrayList<Integer> tmp = getCarteSelectioner();
-        fin = attaquer(tmp);
-
-        if(fin){
-
+        if(attaquer(carteSelectionée)){
             String msg = "AI:";
-
-            for(Integer i : tmp){
-                msg += tmp + ";";
+            for(Integer i : carteSelectionée){
+                msg += carteSelectionée + ";";
             }
-
             msg = msg.substring(0,msg.length()-1);
-
             messages.add(msg);
-
             finTour();
         }else{
             //Erreur msg: attaque imposible
+            Toast.makeText(this,"L'attaque indirect a échoué",Toast.LENGTH_SHORT).show();
+            return;
         }
     }
 
 
     public void actionButtonReculer(){
-
-        boolean fin = false;
         int nb = -1;
 
         if(getCarteSelectioner().size()>1){
-            //Erreur msg : Une seul carte doit étre séléctionner
+            //Erreur msg : Une seul carte doit être sélectionné
+            Toast.makeText(this,"Reculer ne nécessite qu'une carte",Toast.LENGTH_SHORT).show();
+            return ;
         }else {
             nb = getCarteSelectioner().get(0);
-            fin = reculer(nb);
         }
 
 
-        if(fin) {
+        if(reculer(nb)) {
             String msg = "R:" + nb;
             messages.add(msg);
 
             finTour();
         }else{
             //Erreur msg : Déplacement imposible
+            Toast.makeText(this,"Reculer est impossible",Toast.LENGTH_SHORT).show();
+            return ;
         }
     }
 
     public void actionButtonAvancer(){
-
-        boolean fin = false;
         int nb = -1;
 
         if(getCarteSelectioner().size()>1){
-            //Erreur msg : Une seul carte doit étre séléctionner
+            //Erreur msg : Une seul carte doit être sélectionné
+            Toast.makeText(this,"Avancer ne nécessite qu'une carte",Toast.LENGTH_SHORT).show();
+            return ;
         }else {
             nb = getCarteSelectioner().get(0);
-            fin = avancer(nb);
         }
 
-        if(fin) {
+        if(avancer(nb)) {
 
             String msg = "A:" + nb;
             messages.add(msg);
@@ -267,68 +371,23 @@ public class Jeu extends AppCompatActivity{
             //Fin du tour
         }else{
             //Erreur msg : Déplacement imposible
+            Toast.makeText(this,"Avancer est impossible",Toast.LENGTH_SHORT).show();
+            return ;
         }
     }
 
-    /* --- Fin actionButton --- */
+    /* -------------- Fin actionButton ---------------------------------------------------------- */
 
+    /*---------------  Fonctions ---------------------------------------------------------------- */
     public ArrayList<Integer> getCarteSelectioner(){
         ArrayList<Integer> ret = new ArrayList<Integer>();
 
-        //TODO Récupéré les toogle buttons
+        //TODO Récupéré les toogle buttons (numeros de cartes)
 
         return ret;
     }
 
-    public ArrayList<String> parse(String message){
-        String[] original = message.split(":");
-        String[] contenu = original[1].split(";");
 
-        ArrayList<String> messageArray = new ArrayList<String>();
-
-        messageArray.add(original[0]);
-        for(int i=0; i <contenu.length;i++){
-            messageArray.add(contenu[i]);
-        }
-
-        return messageArray;
-    }
-
-    public void test(ArrayList<String> msg){
-
-        switch(msg.get(0)){
-            case "D" : // Mise a jour de la pile de défausse
-                    defausseLast = Integer.parseInt(msg.get(1));
-                break;
-
-            case "P" : // Joueur 2 demmande a piocher
-                    String ret = "PR:";
-                    for(Integer i : pioche(j2)){
-                        ret += i + ";";
-                    }
-
-                    ret = ret.substring(0,ret.length()-1);
-                    sendData(ret);
-
-                    String brut = receiveData(); //TODO Verif Syncro 01
-                    sendData("PN:" + pioche.size());
-                break;
-            case "PN" : // Mise a jour de la taille de la pioche
-                    this.taillePioche = Integer.parseInt(msg.get(1));
-                break;
-            case "PR" : // J2 recoit ses cartes
-                    for(int i=1; i<msg.size();i++) {
-                        j2.addCartes(Integer.parseInt(msg.get(i)));
-                    }
-                    sendData("OK"); //TODO Verif Syncro 01
-                break;
-
-            case "PA" : //Affiche le message :"Le coup a été parrer"
-                    //TODO
-                break;
-
-        }
-    }
 
     public void initialisation(){
         initPioche();
